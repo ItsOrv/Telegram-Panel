17fd4a107f628a9c11f3ad9224fe1de6b5e9a287
Orv <pouria.orv@gmail.com>
2025-01-25 21:43:35 +0330

    idk

---
+++ src/Keyboards.py
@@ -0,0 +1,142 @@
+from telethon import Button
+import logging
+
+from src.actions import Actions
+import os
+import logging
+import asyncio
+import json
+from datetime import datetime
+from telethon import TelegramClient, events, Button
+from telethon.errors import SessionPasswordNeededError, FloodWaitError
+from telethon.tl.types import Channel, Chat
+from src.Config import API_ID, API_HASH, CHANNEL_ID, ADMIN_ID
+
+logger = logging.getLogger(__name__)
+
+class Keyboard:
+
+    @staticmethod
+    def start_keyboard():
+        """Returns the start menu keyboard"""
+        return [
+            [Button.inline("Account Management", 'account_management')],
+            [
+                Button.inline("Individual", 'individual_keyboard'),
+                Button.inline("Bulk", 'bulk_operations')
+            ],
+            [Button.inline("Monitor Mode", 'monitor_mode')],
+            [Button.inline("Report", 'report')]
+        ]
+
+    @staticmethod
+    def monitor_keyboard():
+        """Returns the monitor mode keyboard"""
+        return [
+            [
+                Button.inline('Add Keyword', b'add_keyword'),
+                Button.inline('Remove Keyword', b'remove_keyword')
+            ],
+            [
+                Button.inline('Ignore User', b'ignore_user'),
+                Button.inline('Remove Ignore', b'remove_ignore_user')
+            ],
+            [Button.inline("Update Groups", b'update_groups')],
+            [
+                Button.inline('Show Groups', b'show_groups'),
+                Button.inline('Show Keyword', b'Show_keyword')
+            ],
+            [Button.inline("Show Ignores", b'show_ignores')],
+            [Button.inline("Exit", 'exit')]
+        ]
+
+    @staticmethod
+    def bulk_keyboard():
+        """Returns a keyboard with action buttons like like, join, block, message, comment"""
+        return [
+            [Button.inline('Reaction', 'bulk_reaction')],
+            [Button.inline('Poll', 'bulk_poll')],
+            [Button.inline('Join', 'bulk_join')],
+            [Button.inline('Block', 'bulk_block')],
+            [Button.inline('Send pv', 'bulk_send_pv')],
+            [Button.inline('Comment', 'bulk_comment')],
+            [Button.inline("Exit", 'exit')]
+        ]
+
+    @staticmethod
+    def account_management_keyboard():
+        """Returns the keyboard for account management"""
+        return [
+            [Button.inline('Add Account', 'add_account')],
+            [Button.inline('List Accounts', 'list_accounts')],
+            [Button.inline("Exit", 'exit')]
+        ]
+
+    @staticmethod
+    def channel_message_keyboard(message_link, sender_id):
+        """Returns a keyboard with a 'View Message' URL button and 'Ignore' inline button"""
+        return [
+            [Button.url("View Message", url=message_link)],
+            [Button.inline("❌Ignore❌", data=f"ignore_{sender_id}")]
+        ]
+
+    @staticmethod
+    def toggle_and_delete_keyboard(status, session):
+        """Returns a keyboard with 'Disable/Enable' and 'Delete' buttons"""
+        return [
+            [
+                Button.inline(
+                    "❌ Disable" if status == "🟢 Active" else "✅ Enable",
+                    data=f"toggle_{session}"
+                ),
+                Button.inline("🗑 Delete", data=f"delete_{session}")
+            ]
+        ]
+
+    @staticmethod
+    def individual_keyboard():
+        """Returns the keyboard for individual operations"""
+        return [
+            [Button.inline("Send PV", 'send_pv')],
+            [Button.inline("Join", 'join')],
+            [Button.inline("Left", 'left')],
+            [Button.inline("Comment", 'comment')],
+            [Button.inline("Exit", 'exit')]
+        ]
+
+    @staticmethod
+    def report_keyboard():
+        """Returns the keyboard for report"""
+        return [
+            [Button.inline("Show Stats", 'show_stats')],
+            [Button.inline("Exit", 'exit')]
+        ]
+
+    @staticmethod
+    async def show_keyboard(keyboard_name, event=None):
+        """Dynamically returns and shows the requested keyboard based on its name"""
+        keyboards = {
+            'start': Keyboard.start_keyboard(),
+            'monitor': Keyboard.monitor_keyboard(),
+            'bulk': Keyboard.bulk_keyboard(),
+            'account_management': Keyboard.account_management_keyboard(),
+            'channel_message': Keyboard.channel_message_keyboard,
+            'toggle_and_delete': Keyboard.toggle_and_delete_keyboard,
+            'individual_keyboard': Keyboard.individual_keyboard(),
+            'report': Keyboard.report_keyboard(),
+            'bulk_reaction': Actions.prompt_group_action
+        }
+
+        # Return the keyboard if it exists
+        keyboard = keyboards.get(keyboard_name, None)
+
+        if keyboard:
+            if event:
+                # Clear the previous keyboard
+                await event.edit("Please choose an option:", buttons=keyboard)
+            return keyboard
+        else:
+            if event:
+                return event.respond("Sorry, the requested keyboard is not available.")
+            return None
+
